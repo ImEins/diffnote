4efce0b26e4758011bcb7cda78b04cd633415814
ImEins <mehdi.ramtakh@gmail.com>
2025-03-08 18:47:14 +0000

    Config: Add logging configuration parameters

--- src/core/settings.py
+++ src/core/settings.py
@@ -34,6 +34,11 @@ class Settings(BaseSettings):
     FASTAPI_REDOCS_URL: str | None = f'{FASTAPI_API_V1_PATH}/redocs'
     FASTAPI_OPENAPI_URL: str | None = f'{FASTAPI_API_V1_PATH}/openapi'
 
+    # Log
+    LOG_LEVEL: str = 'INFO'
+    LOG_FORMAT: str = '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</> | <lvl>{level: <8}</> | <lvl>{message}</>'
+    LOG_DIR: Path = Path(BASE_PATH) / 'logs'
+
 
 @lru_cache
 def get_settings() -> Settings:
