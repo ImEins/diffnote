38b559051acda82c5714c74fabc60ca061a34e4b
ImEins <mehdi.ramtakh@gmail.com>
2025-03-09 00:07:07 +0000

    Bootstrap: Add FastAPI application setup with lifespan and middleware configuration

---
+++ src/core/bootstrap.py
@@ -0,0 +1,58 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.common.log import setup_logging
+from src.core.database import create_db_tables, dispose_db
+from src.core.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """
+    Lifespan for the FastAPI app.
+
+    All the operations that need to be done when the app starts and shuts down after the request is processed.
+    """
+    await create_db_tables()
+
+    yield
+
+    await dispose_db()
+
+
+def register_app():
+    """
+    Register the FastAPI app with needed configurations and dependencies.
+    """
+    app = FastAPI(
+        title=settings.FASTAPI_TITLE,
+        version=settings.FASTAPI_VERSION,
+        description=settings.FASTAPI_DESCRIPTION,
+        docs_url=settings.FASTAPI_DOCS_URL,
+        redoc_url=settings.FASTAPI_REDOCS_URL,
+        openapi_url=settings.FASTAPI_OPENAPI_URL,
+        lifespan=lifespan,
+    )
+
+    setup_logging()
+
+    register_middleware(app)
+
+    return app
+
+
+def register_middleware(app: FastAPI):
+    """
+    Register the middleware for the FastAPI app.
+    """
+
+    # CORS
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ALLOWED_ORIGINS,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
