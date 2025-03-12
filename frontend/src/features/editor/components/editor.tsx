9f26c5e7b4ceb679868d262fb0feb1493973294e
ImEins <mehdi.ramtakh@gmail.com>
2025-03-12 11:12:21 +0000

    Refactor: Enhance editor menu and editor components with improved UI and icons

--- frontend/src/features/editor/components/editor.tsx
+++ frontend/src/features/editor/components/editor.tsx
@@ -1,73 +1,33 @@
-import { Button } from "@/components/ui/button";
-import { EditorContent, Editor as TiptapEditor } from "@tiptap/react";
-import { History, Save } from "lucide-react";
-import { Menu } from "./menu";
+import { cn } from '@/lib/utils'
+import { EditorContent, Editor as TiptapEditor } from '@tiptap/react'
 
 interface TipTapEditorProps {
-  editor: TiptapEditor | null;
-  title: string;
-  onTitleChange?: (title: string) => void;
-  isSaving: boolean;
-  lastSaved: Date | null;
+	editor: TiptapEditor | null
+	title: string
+	onTitleChange?: (title: string) => void
+	className?: string
 }
 
-export function Editor({
-  editor,
-  title,
-  onTitleChange,
-  isSaving = false,
-  lastSaved,
-}: TipTapEditorProps) {
-  return (
-    <div className="flex flex-col w-full">
-      <div className="flex justify-between items-center">
-        <Menu editor={editor} />
+export function Editor({ editor, title, onTitleChange, className }: TipTapEditorProps) {
+	return (
+		<div className={cn('flex flex-col w-full', className)}>
+			<div className="max-w-3xl w-full mx-auto">
+				<div className="relative">
+					<input
+						value={title}
+						onChange={e => {
+							onTitleChange?.(e.target.value)
+						}}
+						className="outline-none text-4xl font-bold py-6 my-2 min-h-[60px] w-full placeholder:text-gray-400"
+						placeholder="Untitled"
+						autoFocus={title === ''}
+					/>
+				</div>
 
-        <div className="flex items-center gap-x-4">
-          <Button
-            variant="ghost"
-            size="icon"
-            title="Commit version"
-            className="hover:bg-primary/5"
-          >
-            <History className="size-4" />
-          </Button>
-          <Button
-            variant="default"
-            className="text-xs"
-            size="sm"
-            title="Save version"
-          >
-            <Save className="size-4" />
-            Save version
-          </Button>
-
-          {isSaving ? (
-            <div className="text-sm text-muted-foreground">Saving...</div>
-          ) : lastSaved ? (
-            <div className="text-sm text-muted-foreground">
-              Last saved {lastSaved.toLocaleTimeString()}
-            </div>
-          ) : null}
-        </div>
-      </div>
-      <div className="max-w-3xl w-full mx-auto">
-        <div className="relative">
-          <input
-            value={title}
-            onChange={(e) => {
-              onTitleChange?.(e.target.value);
-            }}
-            className="outline-none text-4xl font-bold py-6 mb-2 min-h-[60px] w-full"
-            placeholder="Untitled"
-            autoFocus={title === ""}
-          />
-        </div>
-
-        <div className="relative">
-          <EditorContent editor={editor} />
-        </div>
-      </div>
-    </div>
-  );
+				<div className="relative">
+					<EditorContent editor={editor} />
+				</div>
+			</div>
+		</div>
+	)
 }
--- frontend/src/features/editor/components/menu.tsx
+++ frontend/src/features/editor/components/menu.tsx
@@ -1,201 +1,168 @@
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { Editor } from "@tiptap/react";
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Separator } from '@/components/ui/separator'
+import { cn } from '@/lib/utils'
+import { Editor } from '@tiptap/react'
 import {
-  Bold as BoldIcon,
-  Code as CodeIcon,
-  Heading as HeadingIcon,
-  Italic as ItalicIcon,
-  List,
-  ListOrdered,
-  Redo2,
-  Strikethrough,
-  Undo2,
-} from "lucide-react";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { cn } from "@/lib/utils";
+	ALargeSmall,
+	Bold as BoldIcon,
+	Code as CodeIcon,
+	Heading1,
+	Heading2,
+	Heading3,
+	Italic as ItalicIcon,
+	List,
+	ListOrdered,
+	Redo2,
+	Strikethrough,
+	Undo2,
+} from 'lucide-react'
 
 interface EditorMenuBarProps {
-  editor: Editor | null;
+	editor: Editor | null
 }
 
 export function Menu({ editor }: EditorMenuBarProps) {
-  if (!editor) {
-    return null;
-  }
+	if (!editor) {
+		return null
+	}
 
-  return (
-    <div className="flex gap-x-5">
-      <div className="flex gap-x-4">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="size-6"
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().chain().focus().undo().run()}
-        >
-          <Undo2 />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="size-6"
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().chain().focus().redo().run()}
-        >
-          <Redo2 />
-        </Button>
-      </div>
+	return (
+		<div className="flex gap-x-5">
+			<div className="flex gap-x-4">
+				<Button
+					variant="ghost"
+					size="icon"
+					className="size-6"
+					onClick={() => editor.chain().focus().undo().run()}
+					disabled={!editor.can().chain().focus().undo().run()}
+				>
+					<Undo2 />
+				</Button>
+				<Button
+					variant="ghost"
+					size="icon"
+					className="size-6"
+					onClick={() => editor.chain().focus().redo().run()}
+					disabled={!editor.can().chain().focus().redo().run()}
+				>
+					<Redo2 />
+				</Button>
+			</div>
 
-      <div>
-        <Separator orientation="vertical" />
-      </div>
+			<div>
+				<Separator orientation="vertical" />
+			</div>
 
-      <div className="flex gap-x-4">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          disabled={!editor.can().chain().focus().toggleBold().run()}
-          className={cn(
-            editor.isActive("bold") ? "bg-gray-200 text-gray-800" : "",
-            "size-6"
-          )}
-        >
-          <BoldIcon />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          disabled={!editor.can().chain().focus().toggleItalic().run()}
-          className={cn(
-            editor.isActive("italic") ? "bg-gray-200 text-gray-800" : "",
-            "size-6"
-          )}
-        >
-          <ItalicIcon />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          disabled={!editor.can().chain().focus().toggleStrike().run()}
-          className={cn(
-            editor.isActive("strike") ? "bg-gray-200 text-gray-800" : "",
-            "size-6"
-          )}
-        >
-          <Strikethrough />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          disabled={!editor.can().chain().focus().toggleCode().run()}
-          className={cn(
-            editor.isActive("code") ? "bg-gray-200 text-gray-800" : "",
-            "size-6"
-          )}
-        >
-          <CodeIcon />
-        </Button>
-      </div>
+			<div className="flex gap-x-4">
+				<Button
+					variant="ghost"
+					size="icon"
+					onClick={() => editor.chain().focus().toggleBold().run()}
+					disabled={!editor.can().chain().focus().toggleBold().run()}
+					className={cn(editor.isActive('bold') ? 'bg-gray-200 text-gray-800' : '', 'size-6')}
+				>
+					<BoldIcon />
+				</Button>
+				<Button
+					variant="ghost"
+					size="icon"
+					onClick={() => editor.chain().focus().toggleItalic().run()}
+					disabled={!editor.can().chain().focus().toggleItalic().run()}
+					className={cn(editor.isActive('italic') ? 'bg-gray-200 text-gray-800' : '', 'size-6')}
+				>
+					<ItalicIcon />
+				</Button>
+				<Button
+					variant="ghost"
+					size="icon"
+					onClick={() => editor.chain().focus().toggleStrike().run()}
+					disabled={!editor.can().chain().focus().toggleStrike().run()}
+					className={cn(editor.isActive('strike') ? 'bg-gray-200 text-gray-800' : '', 'size-6')}
+				>
+					<Strikethrough />
+				</Button>
+				<Button
+					variant="ghost"
+					size="icon"
+					onClick={() => editor.chain().focus().toggleCode().run()}
+					disabled={!editor.can().chain().focus().toggleCode().run()}
+					className={cn(editor.isActive('code') ? 'bg-gray-200 text-gray-800' : '', 'size-6')}
+				>
+					<CodeIcon />
+				</Button>
+			</div>
 
-      <div>
-        <Separator orientation="vertical" />
-      </div>
+			<div>
+				<Separator orientation="vertical" />
+			</div>
 
-      <div className="flex gap-x-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="size-6">
-              <HeadingIcon />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem
-              onClick={() =>
-                editor.chain().focus().toggleHeading({ level: 1 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 1 })
-                  ? "bg-gray-200 text-gray-800"
-                  : ""
-              }
-            >
-              H1
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() =>
-                editor.chain().focus().toggleHeading({ level: 2 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 2 })
-                  ? "bg-gray-200 text-gray-800"
-                  : ""
-              }
-            >
-              H2
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() =>
-                editor.chain().focus().toggleHeading({ level: 3 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 3 })
-                  ? "bg-gray-200 text-gray-800"
-                  : ""
-              }
-            >
-              H3
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => editor.chain().focus().setParagraph().run()}
-              className={
-                editor.isActive("paragraph") ? "bg-gray-200 text-gray-800" : ""
-              }
-            >
-              Normal text
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+			<div className="flex gap-x-4">
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" size="icon" className="size-6">
+							{editor.isActive('heading', { level: 1 }) ? (
+								<Heading1 />
+							) : editor.isActive('heading', { level: 2 }) ? (
+								<Heading2 />
+							) : editor.isActive('heading', { level: 3 }) ? (
+								<Heading3 />
+							) : (
+								<ALargeSmall />
+							)}
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuItem
+							onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+							className={editor.isActive('heading', { level: 1 }) ? 'bg-gray-200 text-gray-800' : ''}
+						>
+							<Heading1 /> <span className="text-xs">Heading 1</span>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+							className={editor.isActive('heading', { level: 2 }) ? 'bg-gray-200 text-gray-800' : ''}
+						>
+							<Heading2 /> <span className="text-xs">Heading 2</span>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+							className={editor.isActive('heading', { level: 3 }) ? 'bg-gray-200 text-gray-800' : ''}
+						>
+							<Heading3 /> <span className="text-xs">Heading 3</span>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => editor.chain().focus().setParagraph().run()}
+							className={editor.isActive('paragraph') ? 'bg-gray-200 text-gray-800' : ''}
+						>
+							<ALargeSmall /> <span className="text-xs">Paragraph</span>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
 
-      <div>
-        <Separator orientation="vertical" />
-      </div>
+			<div>
+				<Separator orientation="vertical" />
+			</div>
 
-      <div className="flex gap-x-4">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={cn(
-            editor.isActive("bulletList") ? "bg-gray-200 text-gray-800" : "",
-            "size-6"
-          )}
-        >
-          <List />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={cn(
-            editor.isActive("orderedList") ? "bg-gray-200 text-gray-800" : "",
-            "size-6"
-          )}
-        >
-          <ListOrdered />
-        </Button>
-      </div>
-    </div>
-  );
+			<div className="flex gap-x-4">
+				<Button
+					variant="ghost"
+					size="icon"
+					onClick={() => editor.chain().focus().toggleBulletList().run()}
+					className={cn(editor.isActive('bulletList') ? 'bg-gray-200 text-gray-800' : '', 'size-6')}
+				>
+					<List />
+				</Button>
+				<Button
+					variant="ghost"
+					size="icon"
+					onClick={() => editor.chain().focus().toggleOrderedList().run()}
+					className={cn(editor.isActive('orderedList') ? 'bg-gray-200 text-gray-800' : '', 'size-6')}
+				>
+					<ListOrdered />
+				</Button>
+			</div>
+		</div>
+	)
 }
